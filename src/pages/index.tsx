56e4e016613ad4b65f860df7cfa09c8dc439f7c8
MacielDev <andreraizdetudo@gmail.com>
2022-12-20 20:23:36 -0300

    Iniciando uso da API do Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import style from "../styles/home.module.scss"
+
+import Head from 'next/head'
+import styles from './home.module.scss';
+import { SubscribeButton } from "../components/SubscribeButton";
 
 
 export default function Home() {
   return (
-    <h1 className={style.title}>Hello word Next</h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, wellcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get Access to all the publications<br />
+            <span>for $9.98 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+   
   )
 }
